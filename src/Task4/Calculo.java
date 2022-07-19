9464bb972baa6f42b3ebeaf7c0a72f5310b62b1b
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-19 22:37:24 +0200

    added needed methods in task4

--- src/Task4/Calculo.java
+++ src/Task4/Calculo.java
@@ -60,24 +60,105 @@ public class Calculo {
 	}
 
 	public void resta() {
+		double num1 = 0;
+		double num2 = 0;
+		double resultado = 0;
+		try {
+			num1 = Double.parseDouble(JOptionPane.showInputDialog("introduce el primer numero: "));
+		} catch (NumberFormatException e) {
+			System.out.println("introduce un numero!");
+		}
+		try {
+			num2 = Double.parseDouble(JOptionPane.showInputDialog("introduce el segundo numero: "));
+		} catch (NumberFormatException e) {
+			System.out.println("introduce un numero!");
+		}
+		resultado = num1 - num2;
+		JOptionPane.showMessageDialog(null, "la resta de " + num1 + " menos " + num2 + "es " + resultado);
 
 	}
 
 	public void multiplica() {
+		double num1 = 0;
+		double num2 = 0;
+		double resultado = 0;
+		try {
+			num1 = Double.parseDouble(JOptionPane.showInputDialog("introduce el primer numero: "));
+		} catch (NumberFormatException e) {
+			System.out.println("introduce un numero!");
+		}
+		try {
+			num2 = Double.parseDouble(JOptionPane.showInputDialog("introduce el segundo numero: "));
+		} catch (NumberFormatException e) {
+			System.out.println("introduce un numero!");
+		}
+		resultado = num1 * num2;
+		JOptionPane.showMessageDialog(null, "la multiplicación de " + num1 + " y " + num2 + "es " + resultado);
+
 	}
 
-	public void divide() {
+	public void divide() {// falta controlar la división por 0
+		double num1 = 0;
+		double num2 = 0;
+		double resultado = 0;
+		try {
+			num1 = Double.parseDouble(JOptionPane.showInputDialog("introduce el primer numero: "));
+		} catch (NumberFormatException e) {
+			System.out.println("introduce un numero!");
+		}
+		try {
+			num2 = Double.parseDouble(JOptionPane.showInputDialog("introduce el segundo numero: "));
+		} catch (NumberFormatException e) {
+			System.out.println("introduce un numero!");
+		}
+		resultado = num1 / num2;
+		JOptionPane.showMessageDialog(null, "la división de " + num1 + " entre " + num2 + "es " + resultado);
 
 	}
 
 	public void potencia() {
+		double num1 = 0;
+		double num2 = 0;
+		double resultado = 0;
+		try {
+			num1 = Double.parseDouble(JOptionPane.showInputDialog("introduce el numero que quieres elevar: "));
+		} catch (NumberFormatException e) {
+			System.out.println("introduce un numero!");
+		}
+		try {
+			num2 = Double.parseDouble(JOptionPane.showInputDialog("introduce la potencia a la que quieres elevar: "));
+		} catch (NumberFormatException e) {
+			System.out.println("introduce un numero!");
+		}
+		resultado = Math.pow(num1, num2);
+		JOptionPane.showMessageDialog(null, num1 + " elevado a " + num2 + "es " + resultado);
 
 	}
 
 	public void raizCuadrada() {
+		double num1 = 0;
+		double resultado = 0;
+		try {
+			num1 = Double.parseDouble(JOptionPane.showInputDialog("introduce el numero para saber su raís cuadrada: "));
+		} catch (NumberFormatException e) {
+			System.out.println("introduce un numero!");
+		}
+
+		resultado = Math.sqrt(num1);
+		JOptionPane.showMessageDialog(null, "la raiz cuadrada de " + "es " + resultado);
 	}
 
 	public void raizCubica() {
+		double num1 = 0;
+		double resultado = 0;
+		try {
+			num1 = Double.parseDouble(JOptionPane.showInputDialog("introduce el numero para saber su raíz cúbica: "));
+		} catch (NumberFormatException e) {
+			System.out.println("introduce un numero!");
+		}
+
+		resultado = Math.cbrt(num1);
+		JOptionPane.showMessageDialog(null, "la raiz cuadrada de " + "es " + resultado);
 	}
 
 }
